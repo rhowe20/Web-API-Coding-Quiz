2ff040cdf672caf040011d0079e8f044ec32e2c0
Rebecca Howe <Rebeccahowe20@gmail.com>
2020-12-01 14:00:23 -0800

    remove uneeded text from html & fix js

--- script.js
+++ script.js
@@ -1,52 +1,85 @@
 var timeEl = document.querySelector("#timer");
-var buttonEl = document.querySelector('#start-button')
+var buttonEl = document.querySelector('#start-button');
+var questArea = document.querySelector('#question-area');
+var score = 0;
+var qIndex = 0;
 
+
+// Array of questions, choices, and answers
 var questions = [{
-    question: 'question1',
-    choices: ['strings', 'booleans1', 'alerts', 'numbers'],
-    answer: 'alerts'
+    q: 'Commonly used data types DO NOT include:',
+    ch: ['strings', 'booleans', 'alerts', 'numbers'],
+    a: 'alerts'
 },{
-    question: 'question2',
-    choices: ['quotes1', 'curleyBrackets1', 'parentheses1', 'squareBrackets'],
-    answer: 'curleyBrackets1',
+    q: 'The condition in an if/else statement is enclosed with:',
+    ch: ['quotes', 'curley brackets', 'parentheses', 'square brackets'],
+    a: 'curley brackets',
 },{
-    question: 'question3',
-    choices: ['numberStrings', 'otherArr', 'booleans2', 'allAbove'],
-    answer: 'allAbove',
+    q: 'Arrays in JavaScript can be used to store ___:',
+    ch: ['number strings', 'other array', 'booleans', 'all of the above'],
+    a: 'all of the above',
 },{
-    question: 'question4',
-    choices: ['commas', 'curlyBrackets2', 'quotes2', 'parentheses2'],
-    answer: 'parentheses2',
+    q: 'String vaules must be enclosed within ____ when being assigned to variables.',
+    ch: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+    a: 'parentheses',
 },{
-    question: 'question5',
-    choices: ['js', 'terminalBash', 'loops', 'consoleLog'],
-    answer: 'consoleLog',
+    q: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+    ch: ['JavaScript', 'terminal/bash', 'loops', 'console log'],
+    a: 'console log',
 }
 
 
 ];
-var secondsLeft = 60;
+
+// Timer
+var secondsLeft = 75;
 
 function setTime() {
   var timerInterval = setInterval(function() {
     secondsLeft--;
     timeEl.textContent = secondsLeft;
+    if(qIndex === questions.length -1){
+      clearInterval(timerInterval);
+    }
     if(secondsLeft === 0) {
       clearInterval(timerInterval);
     }
   }, 1000);
 }
 
-buttonEl.addEventListener('click', function(){
+
+function renderQuestions(){
+  document.querySelector('#question-header').textContent = questions[qIndex].q;
+  document.querySelector('#button1').textContent = questions[qIndex].ch[0];
+  document.querySelector('#button2').textContent = questions[qIndex].ch[1];
+  document.querySelector('#button3').textContent = questions[qIndex].ch[2];
+  document.querySelector('#button4').textContent = questions[qIndex].ch[3];
+}
+
+buttonEl.addEventListener('click', function(event){
     setTime();
     var startBtn = document.querySelector('#description');
     startBtn.style.display = 'none'
-    document.querySelector('#question1').style.display = 'block';
+    questArea.style.display = 'block';
+    renderQuestions();
+    console.log(event.target)
 });
 
-q1Answer.addEventListener('click', function(){
-    var answer1 = document.querySelector('#question1');
-    answer1.style.display = 'none';
-    document.querySelector('#question2').style.display = 'block';
+questArea.addEventListener('click', function(event){
+  if(qIndex === questions.length -1){
+    return;
+  }
+  if(event.target.type === 'button'){
+    if(event.target.innerText === questions[qIndex].a){
+      console.log(true)
+      score++;
+      qIndex++;
+      renderQuestions();
+    }
+    else{
+      qIndex++;
+      renderQuestions();
+    }
+  }
+})
 
-});
